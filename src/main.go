94df9bed4e5a727b348f1ca4b1a56d4532573da6
Denys Vasyliev <den.vasyliev@gmail.com>
2023-03-21 22:58:24 +0000

    add comments by ChatGPT

--- src/main.go
+++ src/main.go
@@ -26,31 +26,35 @@ type RespD struct {
 var upgrader = websocket.Upgrader{} // use default options
 
 func version(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("gohttps v1.0.0")) //should be in var
+	w.Write([]byte("gohttps v1.0.0")) // should be moved to var from conf
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ok"))
+	w.Write([]byte("ok")) // should be moved to var from conf
 }
 
 func main() {
 
+	// Define handlers for the "/version" and "/healthz" routes
 	http.HandleFunc("/version", version)
 	http.HandleFunc("/healthz", healthz)
 
-	// handle `/` route
+	// Handle the root route "/"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// should be moved to var from conf
+		// Define a message to be returned in the response
 		respM := RespM{"WARN_UNAVAILABLE_MAINTENANCE", "Maintenance", "The service is currently under regular maintenance, please, try again later"}
 		//respD := RespD{"WARN_UNAVAILABLE_DEPLOYMENT", "Deployment", "The service is currently being redeployed and will become available shortly"}
 
+		// Convert the message to JSON
 		js, err := json.Marshal(respM)
 		if err != nil {
 			log.Println("json:", err)
 		}
 
+		// Check if the request header contains "Upgrade"
 		wss := r.Header.Get("Upgrade")
 		if wss != "" {
+			// Upgrade the HTTP connection to a WebSocket connection
 			upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 			c, err := upgrader.Upgrade(w, r, nil)
 			if err != nil {
@@ -75,14 +79,14 @@ func main() {
 			}
 		}
 
+		// Set the response header to indicate that the response is in JSON format
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusServiceUnavailable)
-		io.WriteString(w, string(js))
+		w.WriteHeader(http.StatusServiceUnavailable) // Set the response status code to "Service Unavailable"
+		io.WriteString(w, string(js)) // Write the JSON message to the response body
 
 	})
 
-	// run server on port "9000"
-	// should be moved to var
-	log.Fatal(http.ListenAndServeTLS(":8888", "/tmp/tls/tls.crt", "/tmp/tls/tls.key", nil))
+	// Run the server on port 9000 using TLS
+	log.Fatal(http.ListenAndServeTLS(":9000", "/tmp/tls/tls.crt", "/tmp/tls/tls.key", nil))
 
 }
